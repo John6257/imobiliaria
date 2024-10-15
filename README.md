88bfad4e1825247d4135266c32eaf84c96cfcd19
john leal <voltoja35@gmail.com>
2024-10-15 15:50:49 -0300

    estrutura basica

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# imobiliaria
+# Projeto 3 - Site imobiliaria
+
+Projeto de site de uma imobiliaria, utilizando HTML, CSS (FLEX E GRID) JAVASCRIPT, FETCH, JSON, LocalStorage...
---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+// simula um Json online (banco de dados) //
+const listaImoveis = [
+   {
+    id: 1,
+    titulo: 'Apart. 3 dorm. papicu',
+    descricao: 'lindo ap',
+    valor: '$500.000',
+    area: '90 m²',
+    quartos: 3,
+    tipo: 'apartamento', // apart, casa, terreno
+    localizacao: 'Santos doummont, papicu',
+    mapa: 'link do maps',
+    venda_aluguel: 'venda',
+    finalidade: 'residencial',
+    fotos: [
+        '', // padrao [0]
+        '',
+        '',
+        '',
+        '',
+    ],
+    
+   }
+];
